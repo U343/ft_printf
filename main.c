22f80ec5924fd7093bc9fbc0e650275f33ff8f9b
Benjen Davis <bedavis@mi-f4.21-school.ru>
2020-03-11 14:37:10 +0300

    add files

--- main.c
+++ main.c
@@ -3,11 +3,11 @@
 
 int main(void)
 {
-	char *str = "User";
+	//char *str = "User";
 	printf("------------printf-----------------\n");
-	printf("Hello, %s!\n", str);
+	printf("Hello, %-55.3s!\n", "abcd");
 	printf("------------ft_printf--------------\n");
-	ft_printf("Hello, %s!\n", str);
+	ft_printf("Hello, %-55.3hs!\n", "abcd");
 	printf("-----------------------------------\n");
 	return (0);	
 }
