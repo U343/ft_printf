b1fc76245e24131476937630e79b30d60e29ffe1
Wadd Anton <bondarev.1997@bk.ru>
2020-03-11 17:07:09 +0300

    d_flag support size types

--- src/d_flag.c
+++ src/d_flag.c
@@ -6,7 +6,7 @@
 /*   By: wanton <wanton@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/09 15:37:22 by wanton            #+#    #+#             */
-/*   Updated: 2020/03/11 16:06:57 by wanton           ###   ########.fr       */
+/*   Updated: 2020/03/11 17:02:49 by wanton           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,6 @@ void	print_width(t_printf *p, size_t size)
 	char	*c;
 	size_t 	width;
 	size_t	tmp1;
-	size_t	tmp2;
 
 	width = p->w;
 	if (ft_strrchr(p->flag, '0') && !ft_strrchr(p->flag, '-'))
@@ -38,21 +37,13 @@ void	print_width(t_printf *p, size_t size)
 	{
 		tmp1 = size > (size_t)p->prec ? size : p->prec;
 		while (width-- > tmp1)
-		{
-			//ft_putstr("ok5\n");
 			buffer(p, " ", 1);
-		}
 		width++;
 	}
 	if (ft_strrchr(p->flag, '+'))
 		width--;
-	//printf("c = %s\n", c);
-	//printf("size = %d; width = %d\n", (int)size, width);
 	while (width-- > tmp1)
-	{
-		//ft_putstr("ok5\n");
 		buffer(p, c, 1);
-	}
 }
 
 void	print_round(t_printf *p, size_t size)
@@ -66,30 +57,46 @@ void	print_round(t_printf *p, size_t size)
 		buffer(p, "0", 1);
 }
 
-int		d_flag(t_printf *p)
+/*
+**Function call va_arg for different decimal types
+**       Returned: result of the va_arg 
+*/
+
+long long	check_type_size(t_printf *p)
 {
-	int		n;
+	char	*size;
+
+	size = p->size;
+	if (ft_strcmp(size, "l") == 0)
+		return ((long long)va_arg(p->ap, long));
+	else if (ft_strcmp(size, "h") == 0)
+		return ((long long)va_arg(p->ap, short));
+	else if (ft_strcmp(size, "ll") == 0)
+		return (va_arg(p->ap, long long));
+	else if (ft_strcmp(size, "hh") == 0)
+		return ((long long)va_arg(p->ap, char));
+	else
+		return ((long long)va_arg(p->ap, int));
+}
+
+/*
+**Function for print d, ld, lld, hd and hhd
+**       Returned: 0 if successful
+*/
+
+int			d_flag(t_printf *p)
+{
 	char	*res;
 	size_t	size;
 
-	n = va_arg(p->ap, int);
-	ft_putstr("prec = ");
-	ft_putnbr(p->prec);
-	ft_putchar('\n');
-	res = ft_itoa(n);
+	ft_putstr("in d_flag\n");  //delete
+	res = ft_itoall(check_type_size(p));
 	size = ft_strlen(res);
-	//printf("size = %d:\n", (int)size);
 	if (!ft_strrchr(p->flag, '-'))
-	{
-		//ft_putstr("ok\n");
 		print_width(p, size);
-	}
 	print_round(p, size);
 	add_to_buff(res, p);
 	if (ft_strrchr(p->flag, '-'))
-	{
-		//ft_putstr("ok3\n");
 		print_width(p, size);
-	}
 	return (0);
 }
