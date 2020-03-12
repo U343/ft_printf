9b9521033bad65810e39188df7a6e715776b8e58
Benjen Davis <bedavis@at-k4.21-school.ru>
2020-03-12 18:09:43 +0300

    strings

--- src/s_flag.c
+++ src/s_flag.c
@@ -15,10 +15,25 @@
 int		s_flag(t_printf *p)
 {
 	char *res;
+	int i;
+	int k;
 
+	i = 0;
 	res = va_arg(p->ap, char*);
-	while (*res)
-		buffer(p, res++, 1);
+	if ((p->bit & FL_MINUS) > 0)
+	{
+		while (*res && ((i++ < p->prec) || (p->prec == -1)))
+			buffer(p, res++, 1);
+		while (i++ < p->w)
+			buffer(p, " ", 1);
+	}
+	else // to do here!
+	{
+		k = ((int)ft_strlen(res) >= p->prec) ? -p->prec + p->w : p->w - ft_strlen(res);
+		while (k--)
+			buffer(p, " ", 1);
+		while (*res && (i++ < p->prec))
+			buffer(p, res++, 1);
+	}
 	return (0);
 }
-
