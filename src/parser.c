1bbc2df107c5868d98737d0555ec8a5c64e5f6e1
U343 <bondarev.1997@bk.ru>
2020-05-21 15:33:18 +0300

    fix *

--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@
 /*   By: bedavis <bedavis@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/04 14:55:16 by bedavis           #+#    #+#             */
-/*   Updated: 2020/05/21 13:34:08 by null             ###   ########.fr       */
+/*   Updated: 2020/05/21 15:31:26 by null             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,24 +48,30 @@ void	parse_width(t_printf *p)
 	int		spec;
 	char	*str_spec;
 	
-	if (((*p->format > '0') && (*p->format <= '9')) || *p->format == '*')
+	if (((*p->format > '0') && (*p->format <= '9')) || *p->format == '*' )
 	{
 		p->is_w = 1;
 		if (*p->format == '*')
 		{
-			spec = va_arg(p->ap, int);
-			p->w = spec > 1 ? spec : 1;
-			str_spec = ft_itoa(spec);
-			while ((*str_spec >= '0') && (*str_spec <= '9'))
-				++str_spec;
 			++p->format;
+			if (!ft_isdigit(*p->format))
+			{
+				spec = va_arg(p->ap, int);
+				if (spec < 0)
+				{
+					p->bit |= 1 << 3; // задаю флаг -
+					spec *= -1;
+				}
+				p->w = spec > 1 ? spec : 1;
+				str_spec = ft_itoa(spec);
+				while ((*str_spec >= '0') && (*str_spec <= '9'))
+					++str_spec;
+				return ;
+			}
 		}
-		else
-		{
-			p->w = ft_atoi(p->format) > 1 ? ft_atoi(p->format) : 1;
-			while ((*p->format >= '0') && (*p->format <= '9'))
-				++p->format;
-		}
+		p->w = ft_atoi(p->format) > 1 ? ft_atoi(p->format) : 1;
+		while ((*p->format >= '0') && (*p->format <= '9'))
+			++p->format;
 	}
 }
 
