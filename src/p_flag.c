8b2a17d7121ecb1409a27b762914e962cd54e5b4
Макс Коробейников <maxim@MacBook-Air-Maks.local>
2020-05-22 14:24:16 +0300

    norming

--- src/p_flag.c
+++ src/p_flag.c
@@ -22,29 +22,28 @@ int		my_max(int a, int b)
 
 int		p_flag(t_printf *p)
 {
-	char *str;
-	char *cpy;
-	uintmax_t num;
-	int i;
+	char		*str;
+	char		*cpy;
+	uintmax_t	num;
+	int			i;
 
 	num = (unsigned long)(va_arg(p->ap, unsigned long int));
 	num = (uintmax_t)num;
 	i = 0;
-	if (!(str = ft_itoa_base(num, 16, 32))) {
-	    return (-1);
-    }
+	if (!(str = ft_itoa_base(num, 16, 32)))
+		return (-1);
 	if (p->prec == 0 && !num)
 	{
-			buffer(p, "0x", 2);
-			free(str);
-			return (0);
+		buffer(p, "0x", 2);
+		free(str);
+		return (0);
 	}
 	cpy = str;
 	if ((p->bit & FL_MINUS) > 0)
 	{
 		buffer(p, "0x", 2);
 		while ((p->prec - (int)ft_strlen(str) - i) > 0)
-		{	
+		{
 			buffer(p, "0", 1);
 			i++;
 		}
@@ -67,6 +66,6 @@ int		p_flag(t_printf *p)
 		while (*str)
 			buffer(p, str++, 1);
 	}
-    free(cpy);
+	free(cpy);
 	return (0);
 }
